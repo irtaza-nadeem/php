ec15acc5985d607cdf1b52b52b8143d53feb4aab
Irtaza Nadeem <irtaza.nadeem542@gmail.com>
2019-10-28 05:19:56 -0700

    a

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 echo "TESTING";
 
-print_r($_GET["hub_challange"]);
 
-//$fb = file_get_contents("fb.txt");
-//echo $fb;
+$access_token = "EAAKJh2HgAJQBAOO6N1EN3ZAUBNuOq3EKZCl25WNeM56F0yDufX5bUYjtcCGEc6L7y7qpxrnkzbHfbzGfUgAbQHIovdaKo3NP0yC8abt6OIGIAP6ZASZACcH08WZCT0L9ZCDFvQIEqhhU7iTtXVd8CGjxaxn3F8wqYQz0z5XYWdRfNZAF22qoN8f8XuPMZCDsCTUZD";
+$verify_token = "fb_time_bot";
+$hub_verify_token = null;
+
+if(isset($_REQUEST['hub_challenge'])) {
+    $challenge = $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+
+
+if ($hub_verify_token === $verify_token) {
+    echo $challenge;
+}
